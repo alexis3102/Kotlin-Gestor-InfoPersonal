cb9aeb9d65e234a0d58c4d886ec6a82ff47086f5
alexis3102 <darwinalexiscadavidcanas@gmail.com>
2025-07-31 21:46:15 -0500

    Update Prueba.kt

--- Prueba.kt
+++ Prueba.kt
@@ -1,80 +1,220 @@
-fun main(){
-//    val name = "juan"
-//    println("hola $name")
-    for(i in 1..3) {
+fun main() {
 
-        println("escribe tu nombre completo: ")
-        val nombre = readLine() ?: "invitado"
-        println("registro compelto en mayusculas: ${nombre.uppercase()}")
-        println("registro completo en minusculas: ${nombre.lowercase()}")
-        //el ?: es para indicar que si el usuario le da enter sin escribir nada use "invitado", ya que el sistema no tolera valores null
+    println("¿Cuál es tu nombre completo?")
+    val name = readLine()!!
 
-        println("escribe tu edad: ")
-        val edad = readLine()!!.toIntOrNull()?:-1
+    val nameModificado = name?.uppercase()
 
-        if (edad >= 18) {
-            println("usted es mayor de edad")
-        } else if (edad > 0) {
-            println("edad: $edad años")
-        } else {
-            println("edad inválida")
-        }
+    println("Original: $name")
+    println("Modificado: $nameModificado")
 
-        //!! esto le dice al lenguaje que si va resivir un valor y que no va ser null y el toInt es para que sea un entero
-        println("escriba su altura: ")
-        val altura = readLine()?.toFloatOrNull() ?: 1f
-        // ? = es para valores null, por si lo llega a resivir no salte error
-        // en 0f, si es null su valor es 0 y es double, pero si quiero que sea un float se le pone una f al final = 0f
+    println("escribe tu edad")
+    var edad = readLine()!!.toInt()
+    // !! le dice al programa que si es null que salte error pero de contrario que lo use normal como double
+    if (edad < 0){
+        println("edad invalida")
+    }else{
+        println("su edad es valida")
+    }
+    if (edad > 18){
+        println("usted es mayor de edad")
+    }else{
+        println("usted es menor de edad")
+    }
 
-        if (altura>0){
-            println("altura: $altura m")
-        }else{
-            println("su altura es invalida")
-        }
+    println("escriba su altura")
+    val altura = readLine() !!.toDouble()
+    if (altura > 0){
+        println("su altura es valida")
+    }else{
+        println("su altura es invalida")
+    }
 
-        println("escribe tu peso: ")
-        val peso = readLine()?.toFloatOrNull() ?: 0f
+    println("escriba su peso")
+    val peso = readLine()!!.toDouble()
+    if (peso > 0){
+        println("su peso es valido")
+    }else{
+        println("su peso es invalido")
+    }
 
-        if (peso>0){
-            println("peso: $peso kg")
-        }else{
-            println("su peso es invalido")
-        }
+    var IMC = (peso/(altura*altura))
+    println("su IMC es: $IMC")
 
-        println("escribe tu Email: ")
-        val email = readLine() ?: "no hay gmail"
-        if (email.contains("@")&& email.contains(".")){
-            //email.contains("@") si devuelve true si la cadena contiene @
-            //email.contains(".") si devuelve true si la cadena contiene un punto
-            // &&: si ambas condiciones son verdaderas
-            println("email valido: $email")
-        }else{
-            println("su email es invalido")
-        }
+    println("escriba su gmail")
+    val email = readLine()!!
 
-        val dominio = email.substringAfter("@")
-        println("Dominio del email: $dominio")
+    val dominio = email.substringAfter("@")
+    println("Dominio del email: $dominio")
+    //Valida que el email tenga al menos un “@” y un “.”.
+  val result = email.contains("@") && email.contains(".")
+    if (result == true){
+        println("su correo es valido")
+    }else{
+        println("su correo no es valido")
+    }
+    println("escriba sue ciudad")
+    val ciudad = readLine()!!
 
-        println("escriba tu ciudad de residencia: ")
-        val ciudad = readLine() ?: "no hay ubicacion registrada"
+    val resumen = """ 
+        //////////////////////////////////////////////////
+        su nombre es: $name
+        su nombre en mayusculas: ${name.uppercase()}
+        su nombre en minusculas: ${name.lowercase()}
+        su edad es: $edad años
+        su altura es: $altura m
+        su peso es: $peso kg
+        email es: $email
+        su dominio es: $dominio
+        ciudad es: $ciudad
+        --------------------------------------------------
+    """.trimIndent()
 
-        val imc = peso / (altura*altura)
-        println("su IMC es: $imc")
+    println(resumen)
 
+    println("¿Cuál es tu nombre completo?")
+    val name2 = readLine()!!
 
-        val resumen = """
-            ----------------------------------
-            resumen del usuario #$i
-            Nombre original: $nombre
-            Nombre MAYÚSCULAS: ${nombre.uppercase()}
-            Nombre minúsculas: ${nombre.lowercase()}
-            Edad: $edad años
-            Altura: $altura m
-            Peso: $peso kg
-            IMC: $imc
-            Email: $email
-            Dominio del email: $dominio
-            Ciudad: $ciudad
+    val nameModificado2 = name2?.uppercase()
+
+    println("Original: $name2")
+    println("Modificado: $nameModificado2")
+
+    println("escribe tu edad")
+    var edad2 = readLine()!!.toInt()
+    // !! le dice al programa que si es null que salte error pero de contrario que lo use normal como double
+    if (edad2 < 0){
+        println("edad invalida")
+    }else{
+        println("su edad es valida")
+    }
+    if (edad2 > 18){
+        println("usted es mayor de edad")
+    }else{
+        println("usted es menor de edad")
+    }
+
+    println("escriba su altura")
+    val altura2 = readLine() !!.toDouble()
+    if (altura > 0){
+        println("su altura es valida")
+    }else{
+        println("su altura es invalida")
+    }
+
+    println("escriba su peso")
+    val peso2 = readLine()!!.toDouble()
+    if (peso2 > 0){
+        println("su peso es valido")
+    }else{
+        println("su peso es invalido")
+    }
+
+    var IMC2 = (peso/(altura*altura))
+    println("su IMC es: $IMC2")
+
+    println("escriba su gmail")
+    val email2 = readLine()!!
+
+    val dominio2 = email.substringAfter("@")
+    println("Dominio del email: $dominio2")
+    //Valida que el email tenga al menos un “@” y un “.”.
+    val result2 = email.contains("@") && email2.contains(".")
+    if (result2 == true){
+        println("su correo es valido")
+    }else{
+        println("su correo no es valido")
+    }
+    println("escriba sue ciudad")
+    val ciudad2 = readLine()!!
+
+    val resumen2 = """ 
+        //////////////////////////////////////////////////
+        su nombre es: $name2
+        su nombre en mayusculas: ${name2.uppercase()}
+        su nombre en minusculas: ${name2.lowercase()}
+        su edad es: $edad2 años
+        su altura es: $altura2 m
+        su peso es: $peso2 kg
+        email es: $email2
+        su dominio es: $dominio2
+        ciudad es: $ciudad2
+        --------------------------------------------------
+    """.trimIndent()
+
+    println(resumen2)
+
+    println("¿Cuál es tu nombre completo?")
+    val name3 = readLine()!!
+
+    val nameModificado3 = name3?.uppercase()
+
+    println("Original: $name3")
+    println("Modificado: $nameModificado3")
+
+    println("escribe tu edad")
+    var edad3 = readLine()!!.toInt()
+    // !! le dice al programa que si es null que salte error pero de contrario que lo use normal como double
+    if (edad3 < 0){
+        println("edad invalida")
+    }else{
+        println("su edad es valida")
+    }
+    if (edad3 > 18){
+        println("usted es mayor de edad")
+    }else{
+        println("usted es menor de edad")
+    }
+
+    println("escriba su altura")
+    val altura3 = readLine() !!.toDouble()
+    if (altura3 > 0){
+        println("su altura es valida")
+    }else{
+        println("su altura es invalida")
+    }
+
+    println("escriba su peso")
+    val peso3 = readLine()!!.toDouble()
+    if (peso3 > 0){
+        println("su peso es valido")
+    }else{
+        println("su peso es invalido")
+    }
+
+    var IMC3 = (peso/(altura*altura))
+    println("su IMC es: $IMC3")
+
+    println("escriba su gmail")
+    val email3 = readLine()!!
+
+    val dominio3 = email.substringAfter("@")
+    println("Dominio del email: $dominio3")
+    //Valida que el email tenga al menos un “@” y un “.”.
+    val result3 = email.contains("@") && email3.contains(".")
+    if (result3 == true){
+        println("su correo es valido")
+    }else{
+        println("su correo no es valido")
+    }
+    println("escriba sue ciudad")
+    val ciudad3 = readLine()!!
+
+    val resumen3 = """ 
+        //////////////////////////////////////////////////
+        su nombre es: $name3
+        su nombre en mayusculas: ${name3.uppercase()}
+        su nombre en minusculas: ${name3.lowercase()}
+        su edad es: $edad3 años
+        su altura es: $altura3 m
+        su peso es: $peso3 kg
+        email es: $email3
+        su dominio es: $dominio3
+        ciudad es: $ciudad3
+        --------------------------------------------------
+    """.trimIndent()
+    println(resumen3)
+}
             ---------------------------------
             """.trimIndent()
         println(resumen)
